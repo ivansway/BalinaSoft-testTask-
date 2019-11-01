b5c92e8d64031d6d543cd322be08ea6d99431b8c
Burnspb <djsky7777@gmail.com>
2019-11-01 21:39:09 +0300

    Initial Commit

---
+++ BalinaSoft <testTask>/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BalinaSoft <testTask>
+//
+//  Created by Ivan Myrza on 01/11/2019.
+//  Copyright © 2019 Ivan Myrza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
